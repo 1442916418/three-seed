06fe5f0b7ce88a7fed4bd7121b30638b3c6222a2
yushuisheng <1442916418@qq.com>
2023-05-28 09:02:15 +0800

    add: 注释

--- src/entry.js
+++ src/entry.js
@@ -1,26 +1,26 @@
 /**
  * entry.js
- * 
- * This is the first file loaded. It sets up the Renderer, 
- * Scene and Camera. It also starts the render loop and 
+ *
+ * This is the first file loaded. It sets up the Renderer,
+ * Scene and Camera. It also starts the render loop and
  * handles window resizes.
- * 
+ *
  */
 
-import { WebGLRenderer, PerspectiveCamera, Scene, Vector3 } from 'three';
-import SeedScene from './objects/Scene.js';
+import { WebGLRenderer, PerspectiveCamera, Scene, Vector3 } from "three";
+import SeedScene from "./objects/Scene.js";
 
 const scene = new Scene();
 const camera = new PerspectiveCamera();
-const renderer = new WebGLRenderer({antialias: true});
+const renderer = new WebGLRenderer({ antialias: true });
 const seedScene = new SeedScene();
 
 // scene
 scene.add(seedScene);
 
 // camera
-camera.position.set(6,3,-10);
-camera.lookAt(new Vector3(0,0,0));
+camera.position.set(6, 3, -10);
+camera.lookAt(new Vector3(0, 0, 0));
 
 // renderer
 renderer.setPixelRatio(window.devicePixelRatio);
@@ -31,19 +31,19 @@ const onAnimationFrameHandler = (timeStamp) => {
   renderer.render(scene, camera);
   seedScene.update && seedScene.update(timeStamp);
   window.requestAnimationFrame(onAnimationFrameHandler);
-}
+};
 window.requestAnimationFrame(onAnimationFrameHandler);
 
 // resize
-const windowResizeHanlder = () => { 
+const windowResizeHanlder = () => {
   const { innerHeight, innerWidth } = window;
   renderer.setSize(innerWidth, innerHeight);
   camera.aspect = innerWidth / innerHeight;
   camera.updateProjectionMatrix();
 };
 windowResizeHanlder();
-window.addEventListener('resize', windowResizeHanlder);
+window.addEventListener("resize", windowResizeHanlder);
 
 // dom
 document.body.style.margin = 0;
-document.body.appendChild( renderer.domElement );
+document.body.appendChild(renderer.domElement);
--- src/objects/Lights.js
+++ src/objects/Lights.js
@@ -1,21 +1,30 @@
-import { Group, SpotLight, PointLight, AmbientLight, HemisphereLight, Color } from 'three';
+import {
+  Group,
+  SpotLight,
+  PointLight,
+  AmbientLight,
+  HemisphereLight,
+} from "three";
 
 export default class BasicLights extends Group {
   constructor(...args) {
     super(...args);
 
-    const point = new PointLight(0xFFFFFF, 1, 10, 1);
-    const dir = new SpotLight(0xFFFFFF, 0.8, 7, 0.8, 1, 1);
-    const ambi = new AmbientLight( 0x404040 , 0.66);
-    const hemi = new HemisphereLight( 0xffffbb, 0x080820, 1.15 )
+    // 从一个点向各个方向发射的光源。一个常见的例子是模拟一个灯泡发出的光。
+    const point = new PointLight(0xffffff, 1, 10, 1);
+    // 光线从一个点沿一个方向射出，随着光线照射的变远，光线圆锥体的尺寸也逐渐增大。
+    const dir = new SpotLight(0xffffff, 0.8, 7, 0.8, 1, 1);
+    // 环境光会均匀的照亮场景中的所有物体。环境光不能用来投射阴影，因为它没有方向。
+    const ambi = new AmbientLight(0x404040, 0.66);
+    // 光源直接放置于场景之上，光照颜色从天空光线颜色渐变到地面光线颜色。半球光不能投射阴影。
+    const hemi = new HemisphereLight(0xffffbb, 0x080820, 1.15);
 
     dir.position.set(5, 1, 2);
-    dir.target.position.set(0,0,0);
+    dir.target.position.set(0, 0, 0);
 
     point.position.set(0, 1, 5);
     point.intensity = 0.5;
 
     this.add(point, ambi, hemi, dir);
-
   }
 }
